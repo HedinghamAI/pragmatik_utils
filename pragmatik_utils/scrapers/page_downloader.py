74e07ef627aae9fb991e2202d9516408274f6d9f
Mitch <me@mitch.uk>
2021-09-22 21:08:41 +0100

    added a page downloader

---
+++ pragmatik_utils/scrapers/page_downloader.py
@@ -0,0 +1,33 @@
+import os
+import requests
+import logging
+import shutil
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
+
+
+def __download_page(url, file_path):
+    try:
+        if os.path.exists(file_path):
+            return
+        r = requests.get(url, stream=True)
+        if r.status_code == 200:
+            with open(file_path, 'w') as f:
+                f.write(r.content)
+        else:
+            logging.error("failed to get image: {}".format(file_path))
+    except Exception as e:
+        logging.error(e)
+
+
+def fetch_pages(page_list):
+    """Download a list of pages using ThreadPoolExecutor
+    Parameters:
+        image_urls: list(tuple(file_name, image_url))
+    """
+    with tqdm(total=len(page_list)) as pbar:
+        with ThreadPoolExecutor(max_workers=3) as ex:
+            futures = [ex.submit(__download_page, url, file_name) for file_name, url in page_list]
+            for future in as_completed(futures):
+                # result = future.result()
+                pbar.update(1)

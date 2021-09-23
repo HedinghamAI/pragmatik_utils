593e016831b211a75adf85f78cabc3efaf8b7b66
Mitch <me@mitch.uk>
2021-09-23 18:05:45 +0100

    scrape text not content

--- pragmatik_utils/scrapers/page_downloader.py
+++ pragmatik_utils/scrapers/page_downloader.py
@@ -1,7 +1,6 @@
 import os
 import requests
 import logging
-import shutil
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from tqdm import tqdm
 
@@ -13,7 +12,7 @@ def __download_page(url, file_path):
         r = requests.get(url, stream=True)
         if r.status_code == 200:
             with open(file_path, 'w') as f:
-                f.write(r.content)
+                f.write(r.text)
         else:
             logging.error("failed to get image: {}".format(file_path))
     except Exception as e:
@@ -31,3 +30,7 @@ def fetch_pages(page_list):
             for future in as_completed(futures):
                 # result = future.result()
                 pbar.update(1)
+
+
+if __name__ == '__main__':
+    fetch_pages([("page.html", "https://google.com")])
